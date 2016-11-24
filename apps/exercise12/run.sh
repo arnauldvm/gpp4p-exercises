77881b616490af6806df33d90b7c0d3ace9f1c6f
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2016-11-24 11:29:29 +0100

    feat(exercise12): automatically keep track of all profiling data

--- apps/exercise12/run.sh
+++ apps/exercise12/run.sh
@@ -3,4 +3,11 @@ pushd ..
 . setenv.sh
 popd
 
-java -Xprof -classpath target/classes com.kodewerk.profile.CheckIntegerTestHarness
+ts=$(date +%Y%m%d-%H%M%S)
+mkdir -p target/profiles
+prof_file=target/profiles/profile-$ts.txt
+
+prof_opt="-Xprof"
+java $prof_opt -classpath target/classes com.kodewerk.profile.CheckIntegerTestHarness > $prof_file
+
+cat $prof_file
