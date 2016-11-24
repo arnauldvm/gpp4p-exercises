fc2d02d39f249ba2dffcb08d77ea511d2cd9dc90
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2016-11-24 20:49:41 +0100

    fix(exercise12): showing results during run2 (instead of all after run2)

--- apps/exercise12/run2.sh
+++ apps/exercise12/run2.sh
@@ -6,11 +6,12 @@ popd
 ts=$(date +%Y%m%d-%H%M%S)
 mkdir -p target/profiles
 prof_file=target/profiles/profile-$ts.hprof.txt
+out_file=target/profiles/out-$ts.txt
 
 #prof_opt="-agentlib:hprof=cpu=samples,depth=100,interval=20,lineno=y,thread=y,file=out.hprof"
 prof_opt="-agentlib:hprof=cpu=samples,file=$prof_file"
 #prof_opt="-agentlib:hprof=cpu=times,file=$prof_file" # Very slow!
-(set -x; java $prof_opt -classpath target/classes com.kodewerk.profile.CheckIntegerTestHarness > $prof_file.out)
+(set -x; java $prof_opt -classpath target/classes com.kodewerk.profile.CheckIntegerTestHarness | tee $out_file)
 
-tail -30 $prof_file
-cat $prof_file.out
+tail -40 $prof_file
+echo $prof_file
